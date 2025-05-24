45c2b6c1b3f41f1fdab2cb8be8333fe1251d55b9
Abdulkadir Turan <abturan@k1m33s02.42istanbul.com.tr>
2025-05-24 17:50:18 +0300

    bitti

--- srcs/main.c
+++ srcs/main.c
@@ -16,28 +16,20 @@ int	main(int ac, char **av)
 {
 	t_args	args;
 	t_philo	*philos;
-	philos = NULL;
-	char	*error = 0;
+	char	*error;
 
+	philos = NULL;
+	error = 0;
 	error = check_arguments(ac, av);
 	if (ft_strncmp(error, "", 1))
-	{
-		printf("%s\n", error);
-		return (1);
-	}
+		return (printf("%s\n", error), 1);
 	args = set_args(ac, av);
 	philos = malloc(args.number_of_philosophers * sizeof(t_philo));
 	args.philos = philos;
 	if (args.number_of_philosophers == 1)
-	{
-		one_philo(&args, philos);
-		return (0);
-	}
+		return (one_philo(&args, philos), 1);
 	if (philos == NULL || args.error == 1)
-	{
-		printf("Malloc Error.");
-		return (1);
-	}
+		return (printf("Malloc Error."), 1);
 	initalize(&args, philos);
 	free(args.forks);
 	return (0);
